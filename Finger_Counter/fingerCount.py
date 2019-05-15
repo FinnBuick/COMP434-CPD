70e484fb9d72cb5843834dd089e0d25f96c858cc
FinnBuick <finnbuick@gmail.com>
2019-05-15 10:12:48 +1000

    Modularized face detection

--- Finger_Counter/fingerCount.py
+++ Finger_Counter/fingerCount.py
@@ -13,9 +13,9 @@ cv2.namedWindow('Trackbars')
 
 cv2.createTrackbar('L - H', 'Trackbars', 0, 179, nothing)
 cv2.createTrackbar('L - S', 'Trackbars', 47, 255, nothing)
-cv2.createTrackbar('L - V', 'Trackbars', 96, 255, nothing)
-cv2.createTrackbar('U - H', 'Trackbars', 29, 179, nothing)
-cv2.createTrackbar('U - S', 'Trackbars', 129, 255, nothing)
+cv2.createTrackbar('L - V', 'Trackbars', 103, 255, nothing)
+cv2.createTrackbar('U - H', 'Trackbars', 13, 179, nothing)
+cv2.createTrackbar('U - S', 'Trackbars', 255, 255, nothing)
 cv2.createTrackbar('U - V', 'Trackbars', 255, 255, nothing)
 
 def colorFilter(hsv, lower = np.array([0,94,126]),upper = np.array([17,154,255])):
@@ -30,7 +30,7 @@ def defectAngle(start, end, far):
     angle = math.acos((b**2 + c**2 - a**2) / (2*b*c))
     return angle
 
-def countFingers(contour):
+def countFingers(frame, contour):
     """Takes a countour and performs finger detection on it by counting the
     number of convexity defects"""
     hull = cv2.convexHull(contour, returnPoints = False)
@@ -53,71 +53,79 @@ def countFingers(contour):
         """Use the distance between the highest point of the contour and the
         center of the bounding rectangle to determine if a single finger is
         being held up"""
-        top_point = tuple(max_contour[max_contour[:, :, 1].argmin()][0])
+
+        x,y,w,h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
+        center = (x + w/2, y + h/2)
+
+        top_point = tuple(contour[contour[:, :, 1].argmin()][0])
         if (center[1] - top_point[1]) > 100:
             count+=1
 
         return True, count
     return False, 0
 
-while True:
-    ret, frame = cap.read()
-    # Convert from BGR to Gray
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+def faceDetection(gray, hsv):
+        # Detect face and remove it from the image
+        faces = face_cascade.detectMultiScale(gray, 1.05, 5)
 
-    # Detect face and remove it from the image
-    # faces = face_cascade.detectMultiScale(gray, 1.05, 5)
-    #
-    # for (x, y, w, h) in faces:
-    #     cv2.rectangle(hsv, (x,y-40), (x+w, y+h+40), (0,0,0), -1)
-
-    l_h = cv2.getTrackbarPos('L - H', "Trackbars")
-    l_s = cv2.getTrackbarPos('L - S', "Trackbars")
-    l_v = cv2.getTrackbarPos('L - V', "Trackbars")
-    u_h = cv2.getTrackbarPos('U - H', "Trackbars")
-    u_s = cv2.getTrackbarPos('U - S', "Trackbars")
-    u_v = cv2.getTrackbarPos('U - V', "Trackbars")
+        for (x, y, w, h) in faces:
+            cv2.rectangle(hsv, (x,y-40), (x+w, y+h+40), (0,0,0), -1)
 
 
-    # HSV hue sat value
-    lower_red = np.array([l_h,l_s,l_v])
-    upper_red = np.array([u_h,u_s,u_v])
+def main():
+    while True:
+        ret, frame = cap.read()
+        # Convert from BGR to Gray
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # Create a mask for pixel within the upper and lower bounds
-    mask = colorFilter(hsv, lower_red, upper_red)
+        faceDetection(gray, hsv)
 
-    # Preprocessing the mask for contour finding
-    kernel = np.ones((5,5), np.uint8)
-    mask = cv2.dilate(mask,kernel,iterations = 1)
-    mask = cv2.GaussianBlur(mask, (7,7), 100)
-    res = cv2.bitwise_and(frame,frame, mask = mask)
-
-    _, contours, hierachy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(contours) > 0:
-        # draw contours in blue
-        cv2.drawContours(res, contours, -1, (255,255,255), 2)
-
-        # Find the biggest area
-        max_contour = max(contours, key = cv2.contourArea)
-
-        # Draw bounding rect
-        x,y,w,h = cv2.boundingRect(max_contour)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
-        center = (x + w/2, y + h/2)
-
-        fingersPresent, count = countFingers(max_contour)
-        if fingersPresent:
-            font = cv2.FONT_HERSHEY_SIMPLEX
-            cv2.putText(frame, str(count),(10,30), font, 1,(0,0,0),2,cv2.LINE_AA)
+        l_h = cv2.getTrackbarPos('L - H', "Trackbars")
+        l_s = cv2.getTrackbarPos('L - S', "Trackbars")
+        l_v = cv2.getTrackbarPos('L - V', "Trackbars")
+        u_h = cv2.getTrackbarPos('U - H', "Trackbars")
+        u_s = cv2.getTrackbarPos('U - S', "Trackbars")
+        u_v = cv2.getTrackbarPos('U - V', "Trackbars")
 
 
-    cv2.imshow('frame', frame)
-    cv2.imshow('res', res)
-    cv2.imshow('mask', mask)
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+        # HSV hue sat value
+        lower_red = np.array([l_h,l_s,l_v])
+        upper_red = np.array([u_h,u_s,u_v])
 
-cap.release()
-cv2.destroyAllWindows()
+        # Create a mask for pixel within the upper and lower bounds
+        mask = colorFilter(hsv, lower_red, upper_red)
+
+        # Preprocessing the mask for contour finding
+        kernel = np.ones((5,5), np.uint8)
+        mask = cv2.dilate(mask,kernel,iterations = 1)
+        mask = cv2.GaussianBlur(mask, (7,7), 100)
+        res = cv2.bitwise_and(frame,frame, mask = mask)
+
+        contours, hierachy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if len(contours) > 0:
+            # draw contours in blue
+            cv2.drawContours(res, contours, -1, (255,255,255), 2)
+
+            # Find the biggest area
+            max_contour = max(contours, key = cv2.contourArea)
+
+            fingersPresent, count = countFingers(frame, max_contour)
+            if fingersPresent:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                cv2.putText(frame, str(count),(10,30), font, 1,(255,255,255),2,cv2.LINE_AA)
+
+
+        cv2.imshow('frame', frame)
+        cv2.imshow('res', res)
+        cv2.imshow('mask', mask)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
